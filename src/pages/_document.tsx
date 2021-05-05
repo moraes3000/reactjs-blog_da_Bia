ef12e3268e79260447695c25d42aa49ad6a51022
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-05 20:19:49 -0300

    add _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    //TODO
+    return (
+      <>
+        <Html>
+          <Head>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+            <title>Blog da Bia</title>
+          </Head>
+          <body>
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      </>
+    )
+  }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import Header from '../components/header'
+import Header from '../components/Header'
+
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main className=''>
-        <Header />
+        <Header/>
 
         <h1>Bia Sabia que ti amo </h1>
       </main>
