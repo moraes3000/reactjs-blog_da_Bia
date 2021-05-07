5da41ea679e975e106900d74e29cab06926e2867
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-07 14:10:00 -0300

    Header global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
 import { theme } from '../styles/theme'
+import Header from '../components/Header'
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+       <Header />
       <Component {...pageProps} />
     </ChakraProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className=''>
-        <Header />
+       
         <Conteudo />
 
 
