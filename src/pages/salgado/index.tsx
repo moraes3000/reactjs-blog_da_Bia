abc3ca9b90976183b46d4584ed87e3032832efa5
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-07 14:08:32 -0300

    map com item para ser listado

--- src/pages/salgado/index.tsx
+++ src/pages/salgado/index.tsx
@@ -1,34 +1,45 @@
 import { Box } from "@chakra-ui/react"
 import { GetStaticProps } from "next"
+// primic
 import { getPrismicClient } from "../../services/prismic"
 import Prismic from '@prismicio/client'
-
+import { RichText } from 'prismic-dom'
+import React from "react"
+import Link from 'next/link'
 interface Post {
-    uid?: string;
+    slug?: string;
     first_publication_date: string | null;
     title: string;
-    author: string;
-    content: string;
-    thumbnail: string | null;
-    banner: string | null;
-    tag: string | null;
+    author?: string;
+    excerpt: string;
+    updatedAd: string
+    // thumbnail?: string | null;
+    // banner?: string | null;
+    // tag: string | null;
 
 }
+interface PostsProps {
+    posts: Post[]
+}
 
-export default function Salgado() {
+export default function Salgado({ posts }: PostsProps) {
     return (
         <>
-            <Box w="1200px" p={4} m='auto'>
-                <h2>Titulo</h2>
-                <ul>
-                    <li>autor</li>
-                    <li>data de publicaçao</li>
-                </ul>
-                <div>
-                    Conteudo
-                </div>
+            {posts.map(post => (
+                <a href={`salgado/${post.slug}`}>
+                <Box w="1200px" p={4} m='auto' key={post.slug}>
+                    <h2>{post.title}</h2>
+                    <ul>
+                        <li>{post.updatedAd}</li>
+                        <li>{post.author}</li>
+                    </ul>
+                    <div>
+                        {post.excerpt}
+                    </div>
+                </Box>
+                </a>
+            ))}
 
-            </Box>
         </>
     )
 }
@@ -43,9 +54,22 @@ export const getStaticProps: GetStaticProps = async () => {
         pageSize: 2,
     })
 
-    console.log(JSON.stringify(response, null, 2))
+    // console.log(JSON.stringify(response, null, 2))
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? '',//Pegar o conteudo do primeiro paragraph
+            updatedAd: new Date(post.last_publication_date).toLocaleDateString('pt-br', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
 
     return {
-        props: {}
+        props: { posts }
     }
 }
