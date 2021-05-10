92f4195c45653ba4b4f7194b7580d1bdc82f076e
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-10 11:17:10 -0300

    Listagem Salgados

--- src/pages/salgado/index.tsx
+++ src/pages/salgado/index.tsx
@@ -2,7 +2,9 @@ import React from "react"
 import Link from 'next/link'
 import { GetStaticProps } from "next"
 // chakra
-import { Box } from "@chakra-ui/react"
+import { Box, Heading } from "@chakra-ui/react"
+import { Grid, GridItem, Image } from "@chakra-ui/react"
+
 // primic
 import { getPrismicClient } from "../../services/prismic"
 import Prismic from '@prismicio/client'
@@ -15,7 +17,7 @@ interface Post {
     author?: string;
     excerpt: string;
     updatedAd: string
-    // thumbnail?: string | null;
+    thumbnail?: string | null;
     // banner?: string | null;
     // tag: string | null;
 
@@ -27,22 +29,19 @@ interface PostsProps {
 export default function Salgado({ posts }: PostsProps) {
     return (
         <>
-            {posts.map(post => (
-                <Link href={`salgado/${post.slug}`} key={post.slug}>
-                    <a>
-                        <Box w="1200px" p={4} m='auto' >
-                            <h2>{post.title}</h2>
-                            <ul>
-                                <li>{post.updatedAd}</li>
-                                <li>{post.author}</li>
-                            </ul>
-                            <div>
-                                {post.excerpt}
-                            </div>
-                        </Box>
-                    </a>
-                </Link>
-            ))}
+            <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]} gap={6} maxW="1200px" m='auto' mt={8}>
+                {posts.map(post => (
+                    <Box key={post.slug} boxShadow="2xl" justify="center" align="center">
+                        <Link href={`salgado/${post.slug}`}>
+                            <a>
+                                <Image src={post.thumbnail} objectFit="cover" fallbackSrc="https://via.placeholder.com/150" p={4} />
+                                <Heading p={4}>{post.title}</Heading>
+                            </a>
+                        </Link>
+                    </Box>
+                ))}
+            </Grid>
+
 
         </>
     )
@@ -58,13 +57,15 @@ export const getStaticProps: GetStaticProps = async () => {
         pageSize: 2,
     })
 
-    // console.log(JSON.stringify(response, null, 2))
+    console.log(JSON.stringify(response, null, 2))
 
     const posts = response.results.map(post => {
         return {
             slug: post.uid,
             title: RichText.asText(post.data.title),
             excerpt: post.data.content.find(content => content.type == 'paragraph')?.text ?? '',//Pegar o conteudo do primeiro paragraph
+            thumbnail: post.data.thumbnail.url,
+
             updatedAd: new Date(post.last_publication_date).toLocaleDateString('pt-br', {
                 day: '2-digit',
                 month: 'long',
